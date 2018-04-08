e7c6ca8333566b8a156e774f093ce323360a6125
mapledzy <maoozu@outlook.com>
2018-04-08 11:59:02 +0800

    多层数据嵌套生成表格

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,10 @@
-import React, { Component } from 'react';
-import { Button } from 'react-bootstrap';
+import React from 'react';
 // import ajax from './ajax';
 
 import Monster from './monster/monster';
 
-class Oreki extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: 'hello,world!!!',
-    };
-  }
-
-  // onAddressSelect(pIndex, cIndex, aIndex) {
-  //   const { data } = this.state;
-  //   const address = data[pIndex].name
-  //     + data[pIndex].city[cIndex].name
-  //     + data[pIndex].city[cIndex].area[aIndex];
-  //   // console.log(`address: ----->  ${address}`);
-  //   this.address = address;
-  // }
-
-  render() {
-    return (
-      <div>
-        <Button>{this.state.data}</Button>
-        <Monster />
-      </div>
-    );
-  }
-}
-
-export default Oreki;
+export default () => (
+  <div>
+    <Monster />
+  </div>
+);
--- src/monster/monster.jsx
+++ src/monster/monster.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Panel } from 'react-bootstrap';
+import { Table } from 'react-bootstrap';
 
 import datas from '../data/monsters.json';
 
@@ -15,19 +15,49 @@ class Monster extends Component {
   }
 
   render() {
+    const elem = this.list.map(o => (
+      <div key={o}>
+        <h3>{o}</h3>
+        <Table striped bordered condensed>
+
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>名称</th>
+              <th>Lv</th>
+              <th>防御</th>
+              <th>火性</th>
+              <th>冰性</th>
+              <th>光性</th>
+              <th>暗性</th>
+            </tr>
+          </thead>
+
+          <tbody>
+
+            {
+              this.monsters[o].map((i, x) => (
+                <tr key={`${o}-${i.monster_name}-${i.id}`}>
+                  <td>{x}</td>
+                  <td>
+                    {i.monster_name}
+                  </td>
+                  <td>{i.level}</td>
+                  <td>{i.defense}</td>
+                  <td>{i.fire_attr_resistance}</td>
+                  <td>{i.ice_attr_resistance}</td>
+                  <td>{i.light_attr_resistance}</td>
+                  <td>{i.dark_attr_resistance}</td>
+                </tr>
+              ))
+            }
+
+          </tbody>
+        </Table>
+      </div>
+    ));
     return (
-      this.list.map(o => (
-        <div key={o}>
-          <Panel>
-            <Panel.Heading>
-              <Panel.Title componentClass="h3">{o}</Panel.Title>
-            </Panel.Heading>
-            <Panel.Body>
-              {this.monsters[o]['0'].monster_name}
-            </Panel.Body>
-          </Panel>
-        </div>
-      ))
+      elem
     );
   }
 }
